faa8ce6f3de3f4009e5aad57de1665a5fe1ddaba
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-05-15 22:43:29 -0700

    Update headerScript.js

--- files/header/headerScript.js
+++ files/header/headerScript.js
@@ -41,7 +41,7 @@ function openHeaderMenu(){
 
 // Create the Footer and Populate it
 function createFooter(){
-	footer.innerHTML = "<br><div class='spacer'></div><div class='container'><div class='footerLinks'>\
+	footer.innerHTML = "<br><div class='spacer'></div><div class='container' style='margin-bottom:0px;'><div class='footerLinks'>\
 				<div> <h4>Information</h4> <a>Getting to Oras Nou</a> <a>Visitor Information</a> <a>Frequently Asked Questions</a></div> \
 				<div> <h4>Plan Your Visit</h4> <a>Transportation</a> <a>Hotels and Lodging</a> <a>Travel Guides</a> <a>Tourism Blog</a></div> \
 				<div> <h4>Explore</h4> <a href='"+linkHead+"things-to-do'>Things to Do</a> <a>Events</a> <a>Clubbing and Nightlife</a> <a>Art and Museums</a> <a>Attracitons</a></div> </div> \
