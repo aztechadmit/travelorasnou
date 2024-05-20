1e5ca0769f27142864db02ec82f60cf1c8b8ac7e
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-05-19 20:18:45 -0700

    Update attractionItems.js

--- files/attractionItems.js
+++ files/attractionItems.js
@@ -6,7 +6,8 @@ attractionCost = [];
 
 // ------------ ATTRACTION TAGS ------------------------------------------------------------------------------------------------------------------------------------------------------------------ //
 
-	attractionTags = ["Romantic", "Local-Hotspot", "Popular", "Unique", "LGBTQ", "Scenic", "Quiet", "Cultural", "Transit-Accessible", "MDA-Accessible", "Learning", "Prestigious", "Social", "Beach-Access", "Outdoors", "Nature", "Themed", "", "", "", "", "", ""];
+	attractionTags = ["Romantic", "Local-Hotspot", "Popular", "Unique", "LGBTQ", "Scenic", "Quiet", "Cultural", "Transit-Accessible", "MDA-Accessible", "Learning", "Prestigious", "Social", "Beach-Access", "Outdoors", "Nature", "Themed", "Live-Entertainment", \
+			  "Concerts", "", "", "", ""];
 
 	attractionCategory = ["Food-and-Drink", "Sports", "Entertainment", "Leisure", "Hospitality", "Attractions", "Museum", "Shopping", "Amusement-Park", "Resort-Hotel", "", "", "", "", ""];
 	attractionLocation = ["Downtown", "Newtown", "Oldtown", "Industrial-City", "Highland", "Northwood", "Airport-City", "Oceanside", "Riverwood"];
@@ -16,10 +17,14 @@ attractionCost = [];
 
 	// attraction_link already includes https://aztechadmit.github.io/travelorasnou/things-to-do
 
+	// Placeholder image: https://aztechadmit.github.io/travelorasnou/files/images/placeholder.jpg
+
 	// attractionItems[NUM] = "category | location | cost | tags | attraction_name | attraction_photo | attraction_link | description";
 
 	attractionItems[0] = "Amusement-Park | Highland | $$$ | Popular,Unique | Globe-City Resort Highland | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/tts-entrance.jpg | attractions/globe-city-resort-highland | A popular resort with two hotels, a theme-parl, and shopping center";
 	attractionItems[1] = "Resort-Hotel | Oceanside | $$$ | Popular,Beach-Access | Globe-City's Oceanside Resort | https://www.oceanskyresort.com/files/1840/DJI_0063.jpg | attractions/globe-city-oceanside-resort | A beach front resort featuring a small amusement park";
 	attractionItems[2] = "Attractions | Downtown | Free | Scenic,Unique,Free,Quiet | Sunvalley Bridge | https://www.indianriverbridge.com/wp-content/uploads/image.png | attractions/sunvalley-bridge | A scenic overlook crossing the Riverwood valley";
 	attractionItems[3] = "Food-and-Drink | Northwood | $$$ | Unique,Local-Hotspot,Themed | Themepark Cafe | https://www.davidthemingworks.com/EN/images/THE-CAVE-THEME-PARK-RESTAURANT--8-_3wa5c5ed.jpg | attraction_link | A themed restaurant featuring unique dining areas";
+	attractionItems[4] = "Sports | Highland | $$ | Popular,Transit-Accessible,Live-Entertainment,Concerts | Industries Stadium | https://i.redd.it/i-built-this-football-stadium-for-my-minecraft-city-what-do-v0-xbgibw3xc32a1.png?width=1280&format=png&auto=webp&s=565de0e8ac3d98776fc4ba1b0e70636db820b0b5 | attraction_link | Famous stadium for large-scale concerts and sporting events";
+	attractionItems[5] = "Entertainment | Highland | $$ | Transit-Accessible,Live-Entertainment,Concerts | Oras Nou STAGE | https://ca-times.brightspotcdn.com/dims4/default/d230055/2147483647/strip/true/crop/800x420+0+44/resize/1200x630!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F50%2Fff%2F40081cbf4c2498e7212c9788b84c%2F1953.jpg | attraction_link | Famous outdoor concert venue overlooking Riverwood";
 
