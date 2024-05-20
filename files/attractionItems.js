f32a1d3e9d504e82ed0b1c883f5d0463dfd3828e
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-05-19 20:13:19 -0700

    Update attractionItems.js

--- files/attractionItems.js
+++ files/attractionItems.js
@@ -6,7 +6,7 @@ attractionCost = [];
 
 // ------------ ATTRACTION TAGS ------------------------------------------------------------------------------------------------------------------------------------------------------------------ //
 
-	attractionTags = ["Romantic", "Local-Hotspot", "Popular", "Unique", "LGBTQ", "Scenic", "Quiet", "Cultural", "Transit-Accessible", "MDA-Accessible", "Learning", "Prestigious", "Social", "Beach-Access", "Outdoors", "Nature", "", "", "", "", "", "", ""];
+	attractionTags = ["Romantic", "Local-Hotspot", "Popular", "Unique", "LGBTQ", "Scenic", "Quiet", "Cultural", "Transit-Accessible", "MDA-Accessible", "Learning", "Prestigious", "Social", "Beach-Access", "Outdoors", "Nature", "Themed", "", "", "", "", "", ""];
 
 	attractionCategory = ["Food-and-Drink", "Sports", "Entertainment", "Leisure", "Hospitality", "Attractions", "Museum", "Shopping", "Amusement-Park", "Resort-Hotel", "", "", "", "", ""];
 	attractionLocation = ["Downtown", "Newtown", "Oldtown", "Industrial-City", "Highland", "Northwood", "Airport-City", "Oceanside", "Riverwood"];
@@ -16,8 +16,10 @@ attractionCost = [];
 
 	// attraction_link already includes https://aztechadmit.github.io/travelorasnou/things-to-do
 
-	// attractionItems[NUM] = "category | location | cost | tags | attraction_name | attraction_photo | attraction_link";
+	// attractionItems[NUM] = "category | location | cost | tags | attraction_name | attraction_photo | attraction_link | description";
 
-	attractionItems[0] = "Amusement-Park | Highland | $$$ | Popular,Unique | Globe-City Resort Highland | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/tts-entrance.jpg | attractions/globe-city-resort-highland";
-	attractionItems[1] = "Resort-Hotel | Oceanside | $$$ | Popular,Beach-Access | Globe-City's Oceanside Resort | https://www.oceanskyresort.com/files/1840/DJI_0063.jpg | attractions/globe-city-oceanside-resort";
-	attractionItems[2] = "Attractions | Downtown | Free | Scenic,Unique,Free,Quiet | Sunvalley Bridge | https://www.indianriverbridge.com/wp-content/uploads/image.png | attractions/sunvalley-bridge";
+	attractionItems[0] = "Amusement-Park | Highland | $$$ | Popular,Unique | Globe-City Resort Highland | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/tts-entrance.jpg | attractions/globe-city-resort-highland | A popular resort with two hotels, a theme-parl, and shopping center";
+	attractionItems[1] = "Resort-Hotel | Oceanside | $$$ | Popular,Beach-Access | Globe-City's Oceanside Resort | https://www.oceanskyresort.com/files/1840/DJI_0063.jpg | attractions/globe-city-oceanside-resort | A beach front resort featuring a small amusement park";
+	attractionItems[2] = "Attractions | Downtown | Free | Scenic,Unique,Free,Quiet | Sunvalley Bridge | https://www.indianriverbridge.com/wp-content/uploads/image.png | attractions/sunvalley-bridge | A scenic overlook crossing the Riverwood valley";
+	attractionItems[3] = "Food-and-Drink | Northwood | $$$ | Unique,Local-Hotspot,Themed | Themepark Cafe | https://www.davidthemingworks.com/EN/images/THE-CAVE-THEME-PARK-RESTAURANT--8-_3wa5c5ed.jpg | attraction_link | A themed restaurant featuring unique dining areas";
+
