ada6abce99389eb3e429e5bbc6f89b6b17b30ed6
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-05-20 09:56:43 -0700

    Update attractionItems.js

--- files/attractionItems.js
+++ files/attractionItems.js
@@ -6,11 +6,14 @@ attractionCost = [];
 
 // ------------ ATTRACTION TAGS ------------------------------------------------------------------------------------------------------------------------------------------------------------------ //
 
-	attractionTags = ["Romantic", "Local-Hotspot", "Popular", "Unique", "LGBTQ", "Scenic", "Quiet", "Cultural", "Transit-Accessible", "MDA-Accessible", "Learning", "Prestigious", "Social", "Beach-Access", "Outdoors", "Nature", "Themed", "Live-Entertainment", 
-			  "Concerts", "Art", "", "", ""];
+	attractionTags = ["Romantic", "Family-oriented", "Adventure", "Historic", "Local Hotspot", "Prestigious", "Scenic", "Educational", "Thrilling", "Cultural", "Entertainment", 
+			  "Relaxing", "Interactive", "Iconic", "Unique", "Foodie", "Active", "Wildlife", "Artistic", "Instagrammable", "Transit Accessible", "MDA Accessible", "Quiet", "Movies"];
 
-	attractionCategory = ["Food-and-Drink", "Sports", "Entertainment", "Leisure", "Hospitality", "Attractions", "Museum", "Shopping", "Amusement-Park", "Resort-Hotel", "University", "", "", "", ""];
-	attractionLocation = ["Downtown", "Newtown", "Oldtown", "Industrial-City", "Highland", "Northwood", "Airport-City", "Oceanside", "Riverwood"];
+	attractionCategory = ["Art & Culture", "Attractions", "Business & Innovation", "Education & Learning", "Events & Festivals", "Family Fun", "Food & Drink", "Landmark", "Nightlife", 
+			      "Outdoor Activities", "Shopping", "Sports & Recreation", "Wellness & Relaxation"];
+
+	attractionLocation = ["Downtown", "Newtown", "Oldtown", "Industrial City", "Highland", "Northwood", "Airport City", "Oceanside", "Riverwood"];
+
 	attractionCost = ["Free", "$", "$$", "$$$", "$$$$"];
 	
 // ------------ ATTRACTION ITEMS ----------------------------------------------------------------------------------------------------------------------------------------------------------------- //
@@ -19,14 +22,21 @@ attractionCost = [];
 
 	// Placeholder image: https://aztechadmit.github.io/travelorasnou/files/images/placeholder.jpg
 
-	// attractionItems[NUM] = "category | location | cost | tags | attraction_name | attraction_photo | attraction_link | description";
+	// attractionItems[NUM] = "name | description | tags | category | location | price | click_link | image_link";
 
-	attractionItems[0] = "Amusement-Park | Highland | $$$ | Popular,Unique | Globe-City Resort Highland | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/tts-entrance.jpg | attractions/globe-city-resort-highland | A popular resort with two hotels, a theme-parl, and shopping center";
-	attractionItems[1] = "Resort-Hotel | Oceanside | $$$ | Popular,Beach-Access | Globe-City's Oceanside Resort | https://www.oceanskyresort.com/files/1840/DJI_0063.jpg | attractions/globe-city-oceanside-resort | A beach front resort featuring a small amusement park";
-	attractionItems[2] = "Attractions | Downtown | Free | Scenic,Unique,Free,Quiet | Sunvalley Bridge | https://www.indianriverbridge.com/wp-content/uploads/image.png | attractions/sunvalley-bridge | A scenic overlook crossing the Riverwood valley";
-	attractionItems[3] = "Food-and-Drink | Northwood | $$$ | Unique,Local-Hotspot,Themed | Themepark Cafe | https://www.davidthemingworks.com/EN/images/THE-CAVE-THEME-PARK-RESTAURANT--8-_3wa5c5ed.jpg | attraction_link | A themed restaurant featuring unique dining areas";
-	attractionItems[4] = "Sports | Highland | $$ | Popular,Transit-Accessible,Live-Entertainment,Concerts | Industries Stadium | https://i.redd.it/i-built-this-football-stadium-for-my-minecraft-city-what-do-v0-xbgibw3xc32a1.png?width=1280&format=png&auto=webp&s=565de0e8ac3d98776fc4ba1b0e70636db820b0b5 | attraction_link | Famous stadium for large-scale concerts and sporting events";
-	attractionItems[5] = "Entertainment | Highland | $$ | Transit-Accessible,Live-Entertainment,Concerts | Oras Nou STAGE | https://ca-times.brightspotcdn.com/dims4/default/d230055/2147483647/strip/true/crop/800x420+0+44/resize/1200x630!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F50%2Fff%2F40081cbf4c2498e7212c9788b84c%2F1953.jpg | attraction_link | Famous outdoor concert venue overlooking Riverwood";
-	attractionItems[6] = "University | Downtown | Free | Learning,Prestegious,Transit-Accessible | University of Minecraftia Oras Nou | https://i.imgur.com/84Iq0cb.jpg | attraction_link | A prestegious university campus and medical center.";
-	attractionItems[7] = "University | Newtown | Free | Learning,Prestegious,Transit-Accessible,Concerts,Art | Oras Nou College of Art and Literature | https://i.pinimg.com/736x/4d/7c/c1/4d7cc11eb0ef75b26bffb53b8b4723af.jpg | attraction_link | A world-class college and performance centers.";
-	attractionItems[8] = "Shopping | Downtown | $$ | Local-Hotspot,Social,MDA-Accessible,Transit-Accessible | Quarts Center-City Mall | https://lamag.com/.image/t_share/MTk3NTU2MDc1OTEzNTUzNjAy/retail-district-level-2.jpg | attraction_link | An enormous shopping, dining, entertainment, and more complex in the heart of downtown";
+	attractionItems[0] = "Coaster Capitol | A large amusement park with many roller coasters and rides | Thrilling,Adventure,Local Hotspot | Attractions | Industrial City | $$ | click_link | https://sf-static.sixflags.com/wp-content/uploads/2020/04/sfa-coaster.jpg";
+	attractionItems[1] = "Globe-City Resort Highland | A popular resort with two hotels, a theme-park, and shopping center | Popular,Unique,All-Day,Themed | Attractions | Highland | $$$ | attractions/globe-city-resort-highland | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/tts-entrance.jpg";
+	attractionItems[2] = "Globe-City's Oceanside Resort | A beach front resort featuring a small amusement park | Popular,Beach-Access | Family Fun | Oceanside | $$$ | attractions/globe-city-oceanside-resort | https://www.oceanskyresort.com/files/1840/DJI_0063.jpg";
+	attractionItems[3] = "Highland Studios | Minecraftia's largest movie studio and famous studio tour | Iconic,Historic,Movies | Attractions | Highland | $ | click_link | https://www.visittheusa.com/sites/default/files/styles/hero_l/public/images/hero_media_image/2019-04/1d9024c65063202d43bdfac0e073dab6.jpeg?h=c1692a99&itok=Ts8Ejha7";
+	attractionItems[4] = "Industries Stadium | Famous stadium for large-scale concerts and sporting events | Popular,Transit-Accessible,Live-Entertainment,Concerts | Sports & Recreation | Highland | $$ | attraction_link | https://i.redd.it/i-built-this-football-stadium-for-my-minecraft-city-what-do-v0-xbgibw3xc32a1.png?width=1280&format=png&auto=webp&s=565de0e8ac3d98776fc4ba1b0e70636db820b0b5";
+	attractionItems[5] = "Northwood Commons | An outdoor shopping plaza framing the gorgeous and historic Northwood Town Hall | Historic,Iconic,MDA Accessible | Shopping | Northwood | $$ | click_link | https://www.irvinecompanyapartments.com/rental-living/wp-content/uploads/2021/03/irvine-spectrum.jpeg";
+	attractionItems[6] = "Northwood Town Hall | A historic landmark to the origins of Oras Nou and Northwood's district office overlooking Riverwood | Historic,Unique,Scenic | Landmark | Northwood | Free | click_link | https://aztechadmit.github.io/travelorasnou/files/images/placeholder.jpg";
+	attractionItems[7] = "Oras Nou College of Art and Literature | A world-class college and performance centers | Educational,Prestigious,Transit Accessible,Concerts,Art | Education & Learning | Newtown | Free | attraction_link | https://i.pinimg.com/736x/4d/7c/c1/4d7cc11eb0ef75b26bffb53b8b4723af.jpg";
+	attractionItems[8] = "Oras Nou Convention Center | A world-famous center of events and festivals | Entertainment,Iconic,Transit Accessible,MDA Accessible | Events & Festivals | Highland | $ | click_link | https://www.exhibitoronline.com/i/pr/ACCNorthExterior917c.jpg";
+	attractionItems[9] = "Oras Nou Public Library | Massive library featured in iconic movie scenes | Historic,Iconic,Movies,Quiet,Unique | Education & Learning | Downtown | Free | click_link | https://cdn.thespaces.com/wp-content/uploads/2023/03/Bibliothe-que-nationale-de-france_HERO.jpg";
+	attractionItems[10] = "Oras Nou STAGE | Famous outdoor concert venue overlooking Riverwood | Transit-Accessible,Live-Entertainment,Concerts,Iconic | Art & Culture | Highland | $$ | attraction_link | https://ca-times.brightspotcdn.com/dims4/default/d230055/2147483647/strip/true/crop/800x420+0+44/resize/1200x630!/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F50%2Fff%2F40081cbf4c2498e7212c9788b84c%2F1953.jpg";
+	attractionItems[11] = "Oras Nou Zoo | Minecraftia's largest Zoo and Environmental Studies Facility | Educational,Nature,Outdoors,All-Day,Wildlife | Attractions | Newtown | $$ | click_link | https://www.9minecraft.net/wp-content/uploads/2022/03/All-Animal-Zoo-Screenshots-1.jpg";
+	attractionItems[12] = "Quartz City-Center Mall | An enormous shopping, dining, entertainment, and more complex in the heart of downtown | Local-Hotspot,Social,MDA-Accessible,Transit-Accessible | Shopping | Downtown | $$ | attraction_link | https://lamag.com/.image/t_share/MTk3NTU2MDc1OTEzNTUzNjAy/retail-district-level-2.jpg";
+	attractionItems[13] = "Sunvalley Bridge | A scenic overlook crossing the Riverwood valley | Scenic,Unique,Free,Quiet,Iconic | Attractions | Downtown | Free | attractions/sunvalley-bridge | https://www.indianriverbridge.com/wp-content/uploads/image.png";
+	attractionItems[14] = "Themepark Cafe | A themed restaurant featuring unique dining areas | Unique,Local-Hotspot,Themed | Food & Drink | Northwood | $$$ | attraction_link | https://www.davidthemingworks.com/EN/images/THE-CAVE-THEME-PARK-RESTAURANT--8-_3wa5c5ed.jpg";
+	attractionItems[15] = "University of Minecraftia Oras Nou | A prestigious university campus and medical center | Educational,Prestigious,Transit Accessible | Education & Learning | Downtown | Free | attraction_link | https://i.imgur.com/84Iq0cb.jpg";
