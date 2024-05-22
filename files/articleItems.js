89296087c6cbce2d5f124a51a2580d3e516fe3dc
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-05-22 08:45:52 -0700

    Create articleItems.js

---
+++ files/articleItems.js
@@ -0,0 +1,19 @@
+articleItems = [];
+articleCategory = [];
+
+//-------- CATEGORIES ----------------------------------------------------------------------------------------------------------- //
+
+	articleCategory = ["none", "Visitor Guides", "Entertainment", "Food & Drink", "Events & Festivals", "Vacation Inspiration", "", "", "", "", ""];
+
+//-------- ITEMS ---------------------------------------------------------------------------------------------------------------- //
+
+	// articleItems[NUM] = "article_title | short_description | author | article_category | image_link | article_link";
+
+	// article_link includes https://aztechadmit.github.io/travelorasnou/articles/
+
+	// <div class='attractionTile'> <img src='IMAGE_LINK'> <div class='txtContent'> <h3>ARTICLE_TITLE</h3><em>ARTICLE_CATEGORY</em>SHORT_DESCRIPTION<div>ARTICLE_AUTHOR</div></div><a class='hiddenLink' href='ARTICLE_LINK'></a></div>
+
+	articleItems[0] = "A Guide to Oras Nou Public Transportation | Learn about our robust transit systems from trains to busses to carpool | Oras Nou Metropolitan Transit Service | Visitor Guides | https://www.sdmts.com/sites/default/files/styles/mts_express/public/bus-mts.jpg?itok=mNIyBU7v | visitor-guides/transportation";	
+	articleItems[1] = "Oras Nou Through Cinema | Take a tour of the cinematic history of Oras Nou on a self-guided adventure around famous movie sets and studios | Joshua Owens | Vacation Inspiration | https://www.gpsmycity.com/img/gd_cover/3079.jpg | entertainment/oras-nou-cinema-history";
+	articleItems[2] = "Understanding the Oras Nou Health System | Things happen, prepare for the worst by understanding your medical options in Oras Nou | Oras Nou Ministry of Health | Visitor Guides | https://s3-prod.modernhealthcare.com/s3fs-public/MAGAZINE_304229969_AR_-1_HKMWCSDZUYRD.jpg | visitor-guides/medical-centers";
+	
