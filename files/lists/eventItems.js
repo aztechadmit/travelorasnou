8186f3a9f6c86a1fbe5e9c709216098dd6248081
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-05-28 10:36:03 -0700

    Create eventItems.js

---
+++ files/lists/eventItems.js
@@ -0,0 +1,24 @@
+eventCategory = [];
+eventLocation = [];
+eventSeason = [];
+eventCost = [];
+eventTags = [];
+
+// ---------------------------------------------------------------------------------------- CATEGORIES ETC -----------------------------------------------------
+
+	eventCategory = ["Music Festival","Cultural Festival","Seasonal Celebration","Special Event","Holiday","Historic"];
+
+	eventTags = ["Local Favorite", "Popular", "Outdoors", "Music", "Live Entertainment", "Televised", "Adult Oriented", "Family Oriented", "Limited Entry"];
+
+	eventSeason = ["Fall", "Winter", "Spring", "Summer", "Halloween", "Holiday Season"];
+
+	eventCost = ["Free Entry","$","$$","$$$","$$$$"];
+	attractionLocation = ["none", "Airport City", "Downtown", "Highland", "Industrial City", "Newtown", "Northwood", "Oceanside", "Oldtown"];
+
+// ---------------------------------------------------------------------------------------- EVENT ITEMS -- -----------------------------------------------------	
+
+//	eventItems[NUM] = "event_name | category | season | location | cost | tags | image_link | click_link";
+
+	eventItems[0] = "Oras Nou Charity Concert by AXIOM Insurance | Music Festival | Spring | Downtown | $ | Local Favorite,Outdoors,Live Entertainment,Televised,Limited Entry | image_link | click_link";
+	eventItems[1] = "Globe-City's SummerFest Celebration | Seasonal Celebration | Summer | Highland | $$$ | Family Oriented,Outdoors,Popular | image_link | click_link";
+	eventItems[2] = "Globe-City's Season of Light Celebration | Seasonal Celebration | Holiday Season | Highland | $$$ | Family Oriented,Outdoors,Popular | image_link | click_link";
