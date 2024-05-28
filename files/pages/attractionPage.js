1ec0302c9d7af37f2fa51fbfc39aa1afe23abcc8
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-05-28 10:55:45 -0700

    Create attractionPage.js

---
+++ files/pages/attractionPage.js
@@ -0,0 +1,43 @@
+attractionPageTitle = []; // Title of the page to display
+attractionPageDescription = []l // Summary of the Attraction / What it is
+attractionPageMore = []; // More general information - not necessary (if blank section will be ommited)
+attractionPageTags = []; // Tags pertaining to this attraction
+
+attractionPageBanner = []; // Image link for the banner of the page
+attractionPageImages = []; // List of two image links
+
+attractionPageLink = []; // Link to the official website of this attraction
+attractionPageSimilar = []; // Input the category of this attraction to search for similar ones
+attractionSeeAlso = []; // Links other related attractions - will show as hyperlinks (or buttons idk yet)
+
+/* NEW ELEMENT:
+
+	attractionPageTitle[NUM] = "page_title";
+	attractionPageDescription[NUM] = "short_description";
+	attractionPageMore[NUM] = "longer_description";
+	attractionPageTags[NUM] = "attraction_tags";
+
+ 	attractionPageLink[NUM] = "attraction_site_link";
+  	attractionPageSimilar[NUM] = "attractio_search_category";
+   	attractionSeeAlso[NUM] = "first_name | first_link || second_name | second_link";
+ 
+ 	attractionPageBanner[NUM] = "link_to_banner_image";
+  	attractionPageImages[NUM] = "image_link_one | image_link_two";
+
+   	/* ----------------------------------------------------------------------------------------------------------------------------------- */
+
+*/
+
+	attractionPageTitle[0] = "Globe-City's Adventure Park";
+	attractionPageDescription[0] = "A fully immersive theme park centered around Adventure and fun for everyone! Part of the famous <a href='https://gcparksandresorts.github.io/gcresorthighland'>Globe-City Resort Highland</a>, a vacation destination featuring a theme park, two hotels, and an electric shopping & dining experience.";
+	attractionPageMore[0] = "Opened on May 26, 2016, Globe-City's Adventure Park has entertained millions of guests looking for fun new adventures. Adventure Park is Globe-City Entertainment's flagship theme park and features a multitude of immersive dark rides, thrilling rollercoasters, and dazzling entertainment.";
+	attractionPageTags[0] = "Family Oriented,Outdoors,Adventure";
+
+ 	attractionPageLink[0] = "https://gcparksandresorts.github.io/gcresorthighland/destinations/adventure-park";
+  	attractionPageSimilar[0] = "Attractions";
+	attractionSeeAlso[0] = "City-Shop at the GC Resort Highland | https://gcparksandresorts.github.io/gcresorthighland/destinations/city-shop || The Aztech Resort | https://gcparksandresorts.github.io/gcresorthighland/destinations/the-aztech-resort || Globe-City's Lakeside Hotel | https://gcparksandresorts.github.io/gcresorthighland/destinations/globe-citys-lakeside-hotel";
+ 
+ 	attractionPageBanner[0] = "https://gcparksandresorts.github.io/gcresorthighland/webresources/images/tts-entrance.jpg";
+  	attractionPageImages[0] = "https://blooloop.com/wp-content/uploads/2016/11/Ghostbusters-triotech.jpg | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/fotd1.jpg";
+
+	/* ----------------------------------------------------------------------------------------------------------------------------------- */
