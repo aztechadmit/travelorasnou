f276660a7617ad0085e01d5aa1278a0a461c75fb
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-05-28 18:09:07 -0700

    Update attractionPage.js

--- files/pages/attractionPage.js
+++ files/pages/attractionPage.js
@@ -24,9 +24,7 @@ attractionSeeAlso = []; // Links other related attractions - will show as hyperl
  	attractionPageBanner[NUM] = "link_to_banner_image";
   	attractionPageImages[NUM] = "image_link_one | image_link_two";
 
-   	IF NO VALUE FOR ELEMENT, USE "nan"
-
-   	/* ----------------------------------------------------------------------------------------------------------------------------------- */
+   	/* ----	IF NO VALUE FOR ELEMENT, USE "nan" ----------------------------------------------------------------------------------------- */
 
 	attractionPageTitle[0] = "Globe-City's Adventure Park";
 	attractionPageDescription[0] = "A fully immersive theme park centered around Adventure and fun for everyone! Part of the famous <a href='https://gcparksandresorts.github.io/gcresorthighland'>Globe-City Resort Highland</a>, a vacation destination featuring a theme park, two hotels, and an electric shopping & dining experience.";
@@ -41,3 +39,17 @@ attractionSeeAlso = []; // Links other related attractions - will show as hyperl
   	attractionPageImages[0] = "https://blooloop.com/wp-content/uploads/2016/11/Ghostbusters-triotech.jpg | https://gcparksandresorts.github.io/gcresorthighland/webresources/images/fotd1.jpg";
 
 	/* ----------------------------------------------------------------------------------------------------------------------------------- */
+
+	attractionPageTitle[1] = "The Bautista Aquarium of Oceanside";
+	attractionPageDescription[1] = "Journey into the deep as you explore vast expanses of aquariums full of life and movement.";
+	attractionPageMore[1] = "The Bautista Aquarium exhibits hundreds of species and thousands of animals across its six major galleries, all of which reside in more than 8 million US gallons of water.<p>The aquarium's notable specimens include whale sharks, beluga whales, California sea lions, bottlenose dolphins, manta rays, sea otters, and tiger sharks.</p>";
+	attractionPageTags[1] = "Family Oriented,Adventure,Educational,Wildlife,Instagrammable,Transit Accessible,MDA Accessible,Romantic,All-Day";
+
+ 	attractionPageLink[1] = "nan";
+  	attractionPageSimilar[1] = "Attractions | Oceanside";
+   	attractionSeeAlso[1] = "Eastpoint Beach | link || Your Moms Hauwse | https://www.pornhub.com";
+ 
+ 	attractionPageBanner[1] = "https://www.georgiaaquarium.org/wp-content/uploads/2019/05/GAQ-Digital-Marketing-201-1.jpg";
+  	attractionPageImages[1] = "https://ourlittlelifestyle.com/storage/2021/09/Georgia-Aquarium-02.jpg | https://res.klook.com/image/upload/c_fill,w_750,h_500/q_80/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/zdhxryz7tekjuorya1nd.jpg";
+
+   	/* ----------------------------------------------------------------------------------------------------------------------------------- */
