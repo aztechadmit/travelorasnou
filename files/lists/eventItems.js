e9d6e24e79b383dbc0c08ac73a3ec600f2d536da
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-05-30 09:53:41 -0700

    Update eventItems.js

--- files/lists/eventItems.js
+++ files/lists/eventItems.js
@@ -17,7 +17,7 @@
 // ---------------------------------------------------------------------------------------- EVENT ITEMS -- -----------------------------------------------------	
 
 //							Months is comma separated list of INTEGER numbers - days correspond with each month -> may,june | 1,2 = May 1st and June 2nd -> 'nan' for days means no specific dates!
-//	eventItems[NUM] = "event_name | category | venue | location | months | days | price | description | tags | image_link | click_link";
+//	eventItems[NUM] = "event_name0 | category1 | venue2 | location3 | months4 | days5 | price6 | description7 | tags8 | image_link9 | click_link10";
 
 	eventItems[0] = "Oras Nou Charity Concert by AXIOM Insurance | Music Festival | Quartz Plaza | Downtown | August | 3-4 | $$ | An annual charity concert supporting Oras Nou Resident Aid programs. Televised across the entire Minecraftian nation, this 2-day event is a holiday on its own! | Local Favorite,Outdoors,Live Entertainment,Televised,Limited Entry | https://www.rollingstone.com/wp-content/uploads/2024/04/coachella-guests-lead-shakira-bizarrap-.jpg?w=1581&h=1054&crop=1 | click_link";
 	eventItems[1] = "Globe-City's SummerFest Celebration | Seasonal Celebration | Globe-City Resort Highland | Highland | June,July,August | nan | $$ | Globe-City's annual summer celebration featuring thrilling rides, exciting entertainment, and fun for everyone! | Popular,Outdoors,Live Entertainment,Fireworks,Family Oriented | https://i.imgur.com/UG5uOpW.jpeg | https://gcparksandresorts.github.io/gcresorthighland/events/summerfest";
