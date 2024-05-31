d216675b85c370b2d2429790947d6b0c7518941f
Aztech Ticketing Services <106356042+aztechadmit@users.noreply.github.com>
2024-05-31 10:05:56 -0700

    Create restaurantItems.js

---
+++ files/lists/restaurantItems.js
@@ -0,0 +1,30 @@
+restaurantItems = [];
+restaurantTags = [];
+restaurantCategory = [];
+restaurantLocation = [];
+restaurantCost = [];
+
+// ------------ restaurant TAGS ------------------------------------------------------------------------------------------------------------------------------------------------------------------ //
+
+	restaurantTags = ["Romantic", "Family Oriented", "Adventure", "Historic", "Local Hotspot", "Prestigious", "Scenic", "Educational", "Thrilling", "Cultural", "Entertainment", 
+			  "Relaxing", "Interactive", "Iconic", "Unique", "Foodie", "Active", "Wildlife", "Artistic", "Instagrammable", "Transit Accessible", "MDA Accessible", "Quiet", "Movies", 
+			  "Music", "Outdoor","LGBTQ", "Broadway", "Technology", "Corporate", "Karaoke", "Beach Access", "All-day"];
+
+	restaurantCategory = ["none", "Art & Culture", "restaurants", "Business & Innovation", "Education & Learning", "Entertainment", "Food & Drink", "Nightlife", 
+			      "Shopping", "Sports & Recreation", "Wellness & Relaxation"];
+
+	restaurantLocation = ["none", "Airport City", "Downtown", "Highland", "Industrial City", "Newtown", "Northwood", "Oceanside", "Oldtown"];
+
+	restaurantCost = ["none","Free", "$", "$$", "$$$", "$$$$"];
+
+	const quickLink = "https://aztechadmit.github.io/travelorasnou/things-to-do/pages/restaurant";
+	
+// ------------ restaurant ITEMS ----------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+	// restaurant_link already includes https://aztechadmit.github.io/travelorasnou/things-to-do
+
+	// Placeholder image: https://aztechadmit.github.io/travelorasnou/files/images/placeholder.jpg
+
+	// restaurantItems[NUM] = "name | description | tags | category | location | price | click_link | image_link";
+
+	// CLICK LINK EXAMPLE: "+quickLink+"?index=NUM
